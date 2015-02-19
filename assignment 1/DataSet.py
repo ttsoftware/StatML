ce68b2e909247c9243da5b4086ed50767f0d1fc7
Troels Thomsen <tt.ttsoftware@gmail.com>
2015-02-19 10:17:27 +0100

    ing

---
+++ assignment 1/DataSet.py
@@ -0,0 +1,5 @@
+
+
+class DataSet(list):
+    def __init__(self, *args, **kwargs):
+        super(DataSet, self).__init__()
--- assignment 1/Normalizer.py
+++ assignment 1/Normalizer.py
@@ -9,10 +9,22 @@ class Normalizer(object):
         self.dataset = dataset
         self.values = LearningDataReader.unpack_params(self.dataset)
 
-    def normalize(self, normalize_function):
+        # We do this in the constructor in order to save time when we normalize input in the future
+        flat_dimensions = []
+        for i in range(len(self.values[0])):
+            flat_dimensions += [map(lambda x: x[i], self.values)]
+
+        self.dimensions_means = []  # mean for each dimension
+        self.dimensions_std = []  # standard deviation for each dimension
+        for dim, dim_values in enumerate(flat_dimensions):
+            self.dimensions_means += [np.mean(dim_values)]
+            self.dimensions_std += [np.std(dim_values)]
+
+    def normalize(self, normalize_function, inputset):
         """
         Return the normalized self.dataset using the given {normalize_function} in each dimension
         :param normalize_function:
+        :param input: must be of type {DataPoint}
         :return:
         """
         normalized_dataset = []
@@ -29,20 +41,11 @@ class Normalizer(object):
 
         return normalized_dataset
 
-    def normalize_means(self):
+    def normalize_means(self, testset):
         """
-        Return normalized version of self.dataset, normalized to each dimension in mean 0 and variance 1.
+        Return normalized version of testset, normalized to each dimension in mean 0 and variance 1.
         Since the variance is standard deviation^2, we can simply subtract the dimension mean and divide by dimension standard deviation in each data point in each dimension.
         This normalization asserts data is gaussian distributed.
         """
-        flat_dimensions = []
-        for i in range(len(self.values[0])):
-            flat_dimensions += [map(lambda x: x[i], self.values)]
 
-        dimensions_means = []  # mean for each dimension
-        dimensions_std = []  # standard deviation for each dimension
-        for dim, dim_values in enumerate(flat_dimensions):
-            dimensions_means += [np.mean(dim_values)]
-            dimensions_std += [np.std(dim_values)]
-
-        return self.normalize(lambda d, x: (x - dimensions_means[d]) / dimensions_std[d])
+        return self.normalize(lambda d, x: (x - self.dimensions_means[d]) / self.dimensions_std[d])
