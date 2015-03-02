5400511015f4fddebc38b59acdb8ecffeedb5f97
RasmusHaarslev <rasmus.haarslev@gmail.com>
2015-03-02 17:55:10 +0100

    loeg

--- assignment 2/thomsen.haarslev.nielsen.py
+++ assignment 2/thomsen.haarslev.nielsen.py
@@ -1,6 +1,9 @@
 from DataReader import DataReader
 from LDAClassifier import LDAClassifier
 from Normalizer import Normalizer
+from Regression import Regression
+import numpy as np
+import matplotlib.pyplot as plt
 
 training_dataset = DataReader.read_data('IrisTrain2014.dt')
 test_dataset = DataReader.read_data('IrisTest2014.dt')
@@ -40,6 +43,41 @@ print 'Normalized test set accuracy: ' + str(normalized_test_accuracy)
 sunspot_training_dataset = DataReader.read_data("sunspotsTrainStatML.dt")
 sunspot_test_dataset 	 = DataReader.read_data("sunspotsTestStatML.dt")
 
-selection1 = map(lambda x: [x.params[2], x.params[3]], sunspot_training_dataset)
-selection2 = map(lambda x: [x.params[4]], sunspot_training_dataset)
-selection3 = map(lambda x: [x.params], sunspot_training_dataset)
+selection1_training = map(lambda x: [x.params[2], x.params[3]], sunspot_training_dataset)
+selection2_training = map(lambda x: [x.params[4]], sunspot_training_dataset)
+selection3_training = map(lambda x: x.params, sunspot_training_dataset)
+
+selection1_test = map(lambda x: [x.params[2], x.params[3]], sunspot_test_dataset)
+selection2_test = map(lambda x: [x.params[4]], sunspot_test_dataset)
+selection3_test = map(lambda x: x.params, sunspot_test_dataset)
+
+regression_training = Regression(sunspot_training_dataset, 1716)
+
+w1_training = Regression.regression(map(lambda x: [1, x], range(1716, 1916)), selection1_training, )
+w2_training = Regression.regression(map(lambda x: [1, x], range(1716, 1916)), selection2_training)
+w3_training = Regression.regression(map(lambda x: [1, x], range(1716, 1916)), selection3_training)
+
+reguessions_training = [Regression.reguession(w2_training, x) for x in range(1716, 1916)]
+reguessions1_test = [Regression.reguession(w1_training, x) for x in range(1916, 2012)]
+reguessions2_test = [Regression.reguession(w2_training, x) for x in range(1916, 2012)]
+reguessions3_test = [Regression.reguession(w3_training, x) for x in range(1916, 2012)]
+
+plt.figure("Regression")
+plt.plot(range(1716, 1916), map(lambda x: sum(x), selection2_training), '.b')
+plt.plot(range(1716, 1916), reguessions_training, 'r')
+plt.show()
+
+plt.figure("Regression")
+plt.plot(range(1916, 2012), map(lambda x: sum(x), selection1_test), '.b')
+plt.plot(range(1916, 2012), reguessions1_test, 'r')
+plt.show()
+
+plt.figure("Regression")
+plt.plot(range(1916, 2012), map(lambda x: sum(x), selection2_test), '.b')
+plt.plot(range(1916, 2012), reguessions2_test, 'r')
+plt.show()
+
+plt.figure("Regression")
+plt.plot(range(1916, 2012), map(lambda x: sum(x), selection3_test), '.b')
+plt.plot(range(1916, 2012), reguessions3_test, 'r')
+plt.show()
