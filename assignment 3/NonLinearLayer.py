83d6fa6ab405826d8e12e03cd4c5602ac0fdd8df
RasmusHaarslev <rasmus.haarslev@gmail.com>
2015-03-10 12:12:25 +0100

    Neural network

--- assignment 3/NonLinearLayer.py
+++ assignment 3/NonLinearLayer.py
@@ -1,10 +1,12 @@
 from pybrain.structure.modules.neuronlayer import NeuronLayer
 
+
 class CustomLayer(NeuronLayer):
-"""Layer implementing the custom function."""
+    """Layer implementing the custom function."""
 
-def _forwardImplementation(self, inbuf, outbuf):
-    outbuf[:] = custom_func_fwd(inbuf)
+    def _forwardImplementation(self, inbuf, outbuf):
+        outbuf[:] = inbuf / (1 + abs(inbuf))
 
-def _backwardImplementation(self, outerr, inerr, outbuf, inbuf):
-    inerr[:] = custom_func_bkwd(outbuf,outerr)
+    def _backwardImplementation(self, outerr, inerr, outbuf, inbuf):
+        raise Exception("NOT YET IMPLEMENTED")
+        #inerr[:] = custom_func_bkwd(outbuf, outerr)
